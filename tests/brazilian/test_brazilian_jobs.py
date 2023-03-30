2ca01509697cdab821882a6076b4298e2bbc7f45
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-30 19:35:20 -0300

    feat: test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+mock_en = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+PATH = 'tests/mocks/brazilians_jobs.csv'
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file(PATH)[0] == mock_en
