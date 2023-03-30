c489ada29e380ab8918216da2a770a29d31b9577
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-30 19:24:35 -0300

    feat: test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+PATH = 'data/jobs.csv'
 
 
 def test_counter():
-    pass
+    jobs_javascript = count_ocurrences(PATH, 'Javascript')
+    jobs_typescript = count_ocurrences(PATH, 'Typescript')
+    jobs_python = count_ocurrences(PATH, 'Python')
+    jobs_java = count_ocurrences(PATH, 'Java')
+
+    assert jobs_javascript == 122
+    assert jobs_typescript == 4
+    assert jobs_python == 1639
+    assert jobs_java == 676
